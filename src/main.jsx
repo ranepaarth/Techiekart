8ebcdda2c46b4c70916f87538db0acf697f4904c
ranepaarth <2019paarth.rane@ves.ac.in>
2023-09-04 18:12:42 +0530

    filterContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { FIlterContextProvider } from "./context/filterContext.jsx";
 import { AppProvider } from "./context/ProductContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <FIlterContextProvider>
+        <App />
+      </FIlterContextProvider>
     </AppProvider>
   </React.StrictMode>
 );
