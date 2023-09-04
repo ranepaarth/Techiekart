3a360a90d5912b541039e677e57ba91a5a10c651
ranepaarth <2019paarth.rane@ves.ac.in>
2023-09-04 18:14:08 +0530

    sorting&view

---
+++ src/reducers/filterReducer.js
@@ -0,0 +1,83 @@
+const filterReducer = (state, action) => {
+  switch (action.type) {
+    case "LOAD_FILTER_PRODUCTS":
+      return {
+        ...state,
+        filter_products: [...action.payload],
+        // all_products: [...action.payload],
+      };
+
+    case "SET_GRID_VIEW":
+      return {
+        ...state,
+        grid_view: true,
+      };
+
+    case "SET_LIST_VIEW":
+      return {
+        ...state,
+        grid_view: false,
+      };
+
+    case "GET_SORT_VALUE":
+      // let userSortValue = document.getElementById("sort");
+      // let sort_value = userSortValue.options[userSortValue.selectedIndex].value;
+      return {
+        ...state,
+        sorting_value: action.payload,
+      };
+
+    case "SORTING_PRODUCTS":
+      let newSortData;
+      // let tempSortData = [...action.payload];
+
+      const { filter_products } = state;
+      let tempSortData = [...filter_products];
+
+      const sortingProducts = (a, b) => {
+        if (state.sorting_value === "lowest") return a.price - b.price;
+        if (state.sorting_value === "highest") return b.price - a.price;
+        if (state.sorting_value === "a-z")
+          return a.title.localeCompare(b.title);
+        if (state.sorting_value === "z-a")
+          return b.title.localeCompare(a.title);
+      };
+
+      // if (state.sorting_value === "lowest") {
+      //   const sortingProducts = (a, b) => {
+      //     return a.price - b.price;
+      //   };
+      //   newSortData = tempSortData.sort(sortingProducts);
+      // }
+
+      // if (state.sorting_value === "highest") {
+      //   const sortingProducts = (a, b) => {
+      //     return b.price - a.price;
+      //   };
+      //   newSortData = tempSortData.sort(sortingProducts);
+      // }
+
+      // if (state.sorting_value === "a-z") {
+      //   newSortData = tempSortData.sort((a, b) => {
+      //     return a.title.localeCompare(b.title);
+      //   });
+      // }
+
+      // if (state.sorting_value === "z-a") {
+      //   newSortData = tempSortData.sort((a, b) => {
+      //     return b.title.localeCompare(a.title);
+      //   });
+      // }
+
+      newSortData = tempSortData.sort(sortingProducts);
+
+      return {
+        ...state,
+        filter_products: newSortData,
+      };
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
