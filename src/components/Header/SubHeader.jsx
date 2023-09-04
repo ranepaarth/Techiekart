177680f9111f67bff498b3b4291fb2e07d839833
ranepaarth <2019paarth.rane@ves.ac.in>
2023-09-04 18:23:23 +0530

    hoverEffect

--- src/components/Header/SubHeader.jsx
+++ src/components/Header/SubHeader.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 const SubHeader = () => {
   return (
-    <nav className='flex justify-between items-center py-2 px-8 cursor-pointer text-xs'>
-       <NavLink to='/'> <span>Home</span></NavLink>
-        <NavLink to='/products'><span>Products</span></NavLink>
+    <nav className="flex justify-between items-center py-3 px-8 cursor-pointer text-xs md:px-24 lg:px-80 md:text-sm">
+      <NavLink to="/">
+      <p className="relative group">
+        <span>Home</span>
+        <span className="absolute -bottom-3 left-0 w-0 h-1 bg-blue-400 transition-all group-hover:w-full"></span>
+      </p>
+      </NavLink>
+      <NavLink to="/products">
+      <p className="relative group">
+        <span>Products</span>
+        <span className="absolute -bottom-3 left-0 w-0 h-1 bg-blue-400 transition-all group-hover:w-full"></span>
+      </p>
+      </NavLink>
+      <p className="relative group">
         <span>About</span>
+        <span className="absolute -bottom-3 left-0 w-0 h-1 bg-blue-400 transition-all group-hover:w-full"></span>
+      </p>
+      <p className="relative group">
         <span>Contact Us</span>
+        <span className="absolute -bottom-3 left-0 w-0 h-1 bg-blue-400 transition-all group-hover:w-full"></span>
+      </p>
     </nav>
-  )
-}
+  );
+};
 
-export default SubHeader
+export default SubHeader;
