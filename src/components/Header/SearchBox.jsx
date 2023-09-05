e86ee49f5da4f20de2771778a6249385de275a9d
ranepaarth <2019paarth.rane@ves.ac.in>
2023-09-05 12:37:05 +0530

    autoComplete&onChange

--- src/components/Header/SearchBox.jsx
+++ src/components/Header/SearchBox.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { useFilterContext } from "../../context/filterContext";
 const SearchBox = () => {
+  const {filters:{text},updateFilterValue} = useFilterContext()
   return (
     <div className="flex items-center gap-1 border-2 border-neutral-400 md:bg-neutral-200 w-[50vw] h-10 rounded-md">
               <span className="text-2xl  text-neutral-500 pl-2">
                 <AiOutlineSearch />
               </span>
               <div className="text-xs flex text-center">
-                <form onSubmit={(e) => e.preventDefault}>
+                <form onSubmit={(e) => e.preventDefault} autoComplete='off'>
                 <input
                   type="text"
                   name="text"
-                  // value={text}
-                  // onChange={updateFilterValue}
+                  value={text}
+                  onChange={updateFilterValue}
                   placeholder="Search for items"
                   className="w-[40vw] bg-transparent placeholder:text-neutral-500  text-neutral-800 md:w-[45vw] outline-none"
                 />
