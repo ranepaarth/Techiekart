6e1b113659e1b54cad8af2b1e8d99203c7a03a4a
ranepaarth <2019paarth.rane@ves.ac.in>
2023-09-05 12:41:06 +0530

    refactor

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,19 +1,10 @@
 import React from "react";
-import { useState } from "react";
-import { useFilterContext } from "../context/filterContext";
 import Categories from "./Categories";
 
 const FilterSection = () => {
-  const {filter_products} = useFilterContext()
-  const allCategories =filter_products.map((item) => item.category)
-  const newCategory = ['all',...new Set(allCategories)]
-
-  const [categories,setCategories] = useState(newCategory)
-  console.log(newCategory)
   return (
-    // <div className='flex flex-col p-4 bg-white h-[40%]'><span>{categories.map(item => item)}</span></div>
-    <div className="flex flex-col items-center">
-      <span className="p-2">
+    <div className="flex flex-col items-center cursor-default">
+      <span className="p-2 cursor-pointer">
         <Categories />
       </span>
     </div>
