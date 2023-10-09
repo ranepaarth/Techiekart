7e6f6815ee8891e8803f1cba2a27542ec7f40f0f
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-09 19:55:05 +0530

    UI update

--- src/components/GridView.jsx
+++ src/components/GridView.jsx
@@ -3,14 +3,29 @@ import { useState } from "react";
 import Product from "./Product";
 import ProductDetailModal from "./ProductDetailModal";
 
+import { AiOutlineClose } from "react-icons/ai";
+
 const GridView = ({ products }) => {
   const [details, setDetails] = useState([]);
   const [show, setShow] = useState(false);
-  if(show) return <><ProductDetailModal details={details}/><button className="z-50 absolute top-14 right-28" onClick={() => setShow(!show)}>close</button></>
+  if (show)
+    return (
+      <>
+        <ProductDetailModal details={details} />
+        <button
+          className="z-50 absolute top-16 right-14 text-lg"
+          onClick={() => setShow(!show)}
+        >
+          <AiOutlineClose />
+        </button>
+      </>
+    );
   return (
     <>
-      <section className="w-[64dvw] lg:w-[75dvw] xl:w-[79dvw] grid md:grid-cols-1 lg:grid-cols-2
-      xl:grid-cols-3 mt-2 xl:ml-[3rem] gap-4 bg-white">
+      <section
+        className="w-[64dvw] lg:w-[75dvw] xl:w-[79dvw] grid md:grid-cols-1 lg:grid-cols-2
+      xl:grid-cols-3 mt-2 xl:ml-[3rem] gap-4 bg-white"
+      >
         {products.map((currElem) => {
           return (
             <article
