9dae562bb0d6f78c8e220653cceaf4e63e0d0f4e
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-11 21:06:54 +0530

    Displayed product brand

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,7 +4,7 @@ const Product = (currElem) => {
     // console.log(currElem);
   return (<div className='flex flex-col gap-4 relative'>
     <span className='text-xs text-white absolute left-0'>{currElem.category}</span>
-    <span className='pt-6 line-clamp-1'>{currElem.title}</span>
+    <span className='pt-6 line-clamp-1'>{currElem.title}<small> ({currElem.brand})</small></span>
     <p>₹{currElem.price}</p>
     </div>
   )
