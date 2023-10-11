c20baafcbdf22af607c187cd705ecb3f27f709f5
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-11 21:43:06 +0530

    added thumbnail as an image for the cart item

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -2,7 +2,7 @@ const cartReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
       let { amount, product } = action.payload;
-      console.log(product)
+      // console.log(product)
 
       //prevent duplicate
 
