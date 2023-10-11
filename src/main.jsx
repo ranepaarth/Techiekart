334254efed0e54a4b4e5efb573586e0fe4d8b87f
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-11 21:19:07 +0530

    Wrapped the App within cartContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { CartContextProvider } from "./context/cartContext.jsx";
+// import { CartProvider } from "./context/cartContext.jsx";
 import { FIlterContextProvider } from "./context/filterContext.jsx";
 import { AppProvider } from "./context/ProductContext.jsx";
 import "./index.css";
@@ -9,7 +11,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
       <FIlterContextProvider>
-        <App />
+        <CartContextProvider>
+          <App />
+        </CartContextProvider>
       </FIlterContextProvider>
     </AppProvider>
   </React.StrictMode>
