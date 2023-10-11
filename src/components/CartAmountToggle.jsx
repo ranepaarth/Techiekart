a9036c1d2b6d7a66a5f135646c924d6c7bb05044
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-11 21:17:06 +0530

    Component to inc\dec the qty of cart items

--- src/components/CartAmountToggle.jsx
+++ src/components/CartAmountToggle.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import {AiOutlineMinus,AiOutlinePlus} from 'react-icons/ai'
-const CartAmountToggle = ({amount,incrementAmount,decrementAmount}) => {
+import React from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const CartAmountToggle = ({ newAmount,incrementAmount,decrementAmount}) => {
+  // console.log(newAmount);
 
-    console.log(amount);
   return (
-    <div className='flex justify-end gap-2 text-large cursor-pointer'>
-        <span className='bg-white flex items-center justify-center px-2' onClick={decrementAmount}><AiOutlineMinus /></span>
-        <span className='flex items-center justify-center px-2'>{amount}</span>
-        <span className='bg-white flex items-center justify-center px-2' onClick={incrementAmount}><AiOutlinePlus /></span>
+    <div className="flex justify-end gap-1 cursor-pointer font-medium">
+      <span
+        className="bg-orange-300 rounded-lg flex items-center justify-center px-2"
+        onClick={decrementAmount}
+      >
+        <AiOutlineMinus />
+      </span>
+      <span className="text-xl flex items-center justify-center px-2 w-8">
+        {newAmount}
+      </span>
+      <span
+        className="bg-orange-300 rounded-lg flex items-center justify-center px-2"
+        onClick={incrementAmount}
+      >
+        <AiOutlinePlus />
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default CartAmountToggle
+export default CartAmountToggle;
