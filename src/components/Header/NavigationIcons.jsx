c5025103029c5f0a4d10966b522140e02ce49257
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-11 21:03:06 +0530

    Displayed cart length

--- src/components/Header/NavigationIcons.jsx
+++ src/components/Header/NavigationIcons.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { FiShoppingBag, FiHeart, FiSettings } from "react-icons/fi";
 import { GoPerson } from "react-icons/go";
+import { useCartContext } from "../../context/cartContext";
 
 const NavigationIcons = () => {
+  const {cart} = useCartContext()
   return (
     <div className="flex items-center gap-2 text-2xl lg:text-3xl lg:gap-5 lg:ml-4 cursor-pointer">
       <NavLink to="/cart">
         <div className="relative py-2">
           <div className="t-0 absolute left-3 lg:left-4">
             <p className="flex h-3 w-3 items-center justify-center rounded-full bg-purple-500 p-2 lg:p-3 lg:text-sm text-xs text-white font-bold">
-              {3}
+              {cart.length}
             </p>
           </div>
           <FiShoppingBag />
