c6a2f0ec68ea2f76aa0a95f3aa9a626f7d4cb424
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-16 21:48:00 +0530

    wishlistContext: functionality to add & remove items in wishList also store the cart data locally

---
+++ src/context/wishlistContext.jsx
@@ -0,0 +1,42 @@
+import { useReducer, useContext, createContext, useEffect } from "react";
+import reducer from "../reducers/wishlistReducer";
+const wishlistContext = createContext();
+
+const getLocalWishListItems = () => {
+    let localCartData = localStorage.getItem("WishListCart")
+    // console.log(localCartData)
+    if(localCartData.length === 0) return [];
+    else return JSON.parse(localCartData)
+}
+
+const initialState = {
+  wishListCart: getLocalWishListItems(),
+  // wishListCart:[]
+};
+
+export const WishlistContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addToWishList = (product,amount=1,addedToWishlist=false) => {
+    // console.log(product);
+    return dispatch({ type: "ADD_TO_WISHLIST", payload: {product,amount,addedToWishlist} });
+  };
+
+  const removeFromWishList = (id) => {
+    return dispatch({type:"REMOVE_FROM_WISHLIST", payload:id})
+  }
+
+  useEffect(()=>{
+    localStorage.setItem("WishListCart",JSON.stringify(state.wishListCart))
+  },[state.wishListCart])
+
+  return (
+    <wishlistContext.Provider value={{ ...state, addToWishList,removeFromWishList }}>
+      {children}
+    </wishlistContext.Provider>
+  );
+};
+
+export const useWishlistContext = () => {
+  return useContext(wishlistContext);
+};
