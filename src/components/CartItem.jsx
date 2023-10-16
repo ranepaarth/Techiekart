c0317320539606baf5edb3249f664701b965cd00
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-16 21:43:08 +0530

    CartItem: designed the CartItem component displaying the Product details

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -4,45 +4,55 @@ import { AiOutlineDelete } from "react-icons/ai";
 import { useCartContext } from "../context/cartContext";
 
 const CartItem = ({ ...product }) => {
+  let { id, name, image, price, amount, stock, brand } = product;
+  const { removeProduct } = useCartContext();
   // console.log(product);
-  let { id, name, image, price, amount } = product;
-  const {removeProduct} = useCartContext()
 
-  let [newAmount,setNewAmount] = useState(amount)
+  let [newAmount, setNewAmount] = useState(amount);
   // console.log(newAmount);
 
-  const incrementAmount = ()=>{
-    setNewAmount(newAmount+1)
-  }
+  const incrementAmount = () => {
+    if (newAmount < stock) setNewAmount(newAmount + 1);
+    else setNewAmount(stock);
+  };
 
-  const decrementAmount = ()=>{
-    if(newAmount>1) setNewAmount(newAmount-1);
-    else newAmount = 1
-  }
+  const decrementAmount = () => {
+    if (newAmount > 1) setNewAmount(newAmount - 1);
+    else newAmount = 1;
+  };
 
   return (
     <>
-      <article className="flex justify-center gap-2 mt-5 cursor-default w-screen px-5">
-        <figure className="flex justify-center items-start w-fit shadow-md border-2 p-1">
-          <img src={image} alt="" className="h-28 w-40"/>
+      <article className="flex justify-center gap-5 mt-5 cursor-default w-screen px-5 border-b pb-5">
+        <figure className="flex justify-center items-start h-fit w-fit shadow-md border-2 p-1 bg-neutral-100">
+          <img src={image} alt="" className="h-28 w-40" />
         </figure>
-        <aside className="flex flex-col gap-1 w-[60%] min-h-full text-start pl-5">
-          <p className="[font-weight:600] text-neutral-400 text-xs">Id: {id}</p>
+        <aside className="flex flex-col gap-2 w-[60%] min-h-full text-start">
           <header className="font-semibold ">
-            <h3 className="text-sm line-clamp-2">{name}</h3>
+            <h3 className="text-sm line-clamp-1">
+              {name} <small>{`(${brand})`}</small>{" "}
+            </h3>
           </header>
           <p className="text-xl font-semibold text-orange-400">₹{price}</p>
+          <small className="[font-weight:400] text-neutral-400 text-xs capitalize">stocks available: {stock}</small>
           <footer className="flex items-center justify-between">
-            <button className="bg-red-400 py-1 px-2 text-white rounded-full" onClick={()=>removeProduct(id)}>
+            <button
+              className="bg-red-400 p-1 text-white rounded-full"
+              onClick={() => removeProduct(id)}
+            >
               <span className="flex items-center gap-2 p-1">
                 <AiOutlineDelete /> <p className="text-sm">Delete</p>
               </span>
             </button>
-            <CartAmountToggle newAmount={newAmount} incrementAmount={incrementAmount} decrementAmount={decrementAmount}/>
+            <CartAmountToggle
+              newAmount={newAmount}
+              incrementAmount={incrementAmount}
+              decrementAmount={decrementAmount}
+            />
           </footer>
         </aside>
       </article>
-      <div className="w-screen border"></div>
+      
     </>
   );
 };
