5f1804b69cdfa609dfe33c7d22242001421eaa54
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-16 21:14:32 +0530

    changed the primary color from pink to purple

--- src/components/Header/SubHeader.jsx
+++ src/components/Header/SubHeader.jsx
@@ -6,22 +6,22 @@ const SubHeader = () => {
       <NavLink to="/">
       <p className="relative group">
         <span>Home</span>
-        <span className="absolute -bottom-3 left-0 w-0 h-[0.2rem] bg-pink-500 transition-all group-hover:w-full"></span>
+        <span className="absolute -bottom-3 left-0 w-0 h-[0.2rem] bg-purple-500 transition-all group-hover:w-full"></span>
       </p>
       </NavLink>
       <NavLink to="/products">
       <p className="relative group">
         <span>Products</span>
-        <span className="absolute -bottom-3 left-0 w-0 h-[0.2rem] bg-pink-500 transition-all group-hover:w-full"></span>
+        <span className="absolute -bottom-3 left-0 w-0 h-[0.2rem] bg-purple-500 transition-all group-hover:w-full"></span>
       </p>
       </NavLink>
       <p className="relative group">
         <span>About</span>
-        <span className="absolute -bottom-3 left-0 w-0 h-[0.2rem] bg-pink-500 transition-all group-hover:w-full"></span>
+        <span className="absolute -bottom-3 left-0 w-0 h-[0.2rem] bg-purple-500 transition-all group-hover:w-full"></span>
       </p>
       <p className="relative group">
         <span>Contact Us</span>
-        <span className="absolute -bottom-3 left-0 w-0 h-[0.2rem] bg-pink-500 transition-all group-hover:w-full"></span>
+        <span className="absolute -bottom-3 left-0 w-0 h-[0.2rem] bg-purple-500 transition-all group-hover:w-full"></span>
       </p>
     </nav>
   );
