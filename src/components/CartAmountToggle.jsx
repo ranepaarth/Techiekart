7af321260f2c69b154acfce999d1258bad207e70
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-16 21:35:16 +0530

    designed a cartItem for the Cart image

--- src/components/CartAmountToggle.jsx
+++ src/components/CartAmountToggle.jsx
@@ -3,19 +3,21 @@ import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const CartAmountToggle = ({ newAmount,incrementAmount,decrementAmount}) => {
   // console.log(newAmount);
 
+  // localStorage.setItem("amount",newAmount);
+  // const localAmount = localStorage.getItem("amount");
+
   return (
     <div className="flex justify-end gap-1 cursor-pointer font-medium">
       <span
-        className="bg-orange-300 rounded-lg flex items-center justify-center px-2"
+        className="bg-orange-300 rounded-full flex items-center justify-center p-2"
         onClick={decrementAmount}
       >
         <AiOutlineMinus />
       </span>
-      <span className="text-xl flex items-center justify-center px-2 w-8">
-        {newAmount}
+      <span className="text-xl flex items-center justify-center px-2 w-7 select-none">{newAmount}
       </span>
       <span
-        className="bg-orange-300 rounded-lg flex items-center justify-center px-2"
+        className="bg-orange-300 rounded-full flex items-center justify-center p-2"
         onClick={incrementAmount}
       >
         <AiOutlinePlus />
