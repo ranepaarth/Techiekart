20b0d883102c14eae5f878119038412b2ffe63fd
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-16 21:07:17 +0530

    included a new API URL

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -16,6 +16,7 @@ const initialState = {
 };
 
 const API = "https://dummyjson.com/products?limit=20";
+// const API = "https://fakestoreapi.com/products";
 // const API = "https://api.pujakaitem.com/api/products"
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
