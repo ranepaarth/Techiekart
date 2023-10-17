3eb65f981790c1b2903b3b59ddfd0427de7d0584
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-17 19:29:20 +0530

    filterReducer: removed commented code

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -7,18 +7,6 @@ const filterReducer = (state, action) => {
         all_products: [...action.payload],
       };
 
-    // case "SET_GRID_VIEW":
-    //   return {
-    //     ...state,
-    //     grid_view: true,
-    //   };
-
-    // case "SET_LIST_VIEW":
-    //   return {
-    //     ...state,
-    //     grid_view: false,
-    //   };
-
     case "GET_SORT_VALUE":
       // let userSortValue = document.getElementById("sort");
       // let sort_value = userSortValue.options[userSortValue.selectedIndex].value;
@@ -112,28 +100,4 @@ const filterReducer = (state, action) => {
 
 export default filterReducer;
 
-// if (state.sorting_value === "lowest") {
-//   const sortingProducts = (a, b) => {
-//     return a.price - b.price;
-//   };
-//   newSortData = tempSortData.sort(sortingProducts);
-// }
 
-// if (state.sorting_value === "highest") {
-//   const sortingProducts = (a, b) => {
-//     return b.price - a.price;
-//   };
-//   newSortData = tempSortData.sort(sortingProducts);
-// }
-
-// if (state.sorting_value === "a-z") {
-//   newSortData = tempSortData.sort((a, b) => {
-//     return a.title.localeCompare(b.title);
-//   });
-// }
-
-// if (state.sorting_value === "z-a") {
-//   newSortData = tempSortData.sort((a, b) => {
-//     return b.title.localeCompare(a.title);
-//   });
-// }
