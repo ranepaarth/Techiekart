785c8e90e0f33d6fec67aa594fa9c9bef9e8d48e
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-20 11:59:53 +0530

    imported the EmptyCartOrList and WishListItem compo

--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -1,39 +1,46 @@
 import React from "react";
-import { useCartContext } from "../context/cartContext";
-import { useWishlistContext } from "../context/wishListContext";
 import { NavLink } from "react-router-dom";
 
-import {AiOutlineClose} from 'react-icons/ai'
-import { IoAddOutline } from "react-icons/io5";
+//Components
+import WishListProductDetailModal from "../components/WishListProductDetailModal";
+import EmptyCartOrList from "../components/EmptyCartOrList";
+import WishListItem from "../components/WishListItem";
+
+//Context
+import { useWishlistContext } from "../context/wishListContext";
 import { useProductContext } from "../context/ProductContext";
-import ProductDetailModal from "../components/ProductDetailModal";
+
+//Icons
+import { IoAddOutline } from "react-icons/io5";
+import { AiOutlineClose } from "react-icons/ai";
 
 const WishList = () => {
-  const { wishListCart, removeFromWishList } = useWishlistContext();
-  const { addToCart } = useCartContext();
-  const {getProductDetails,productDetails,openProductModal,closeProductModal,show} = useProductContext()
+  const { wishListCart } = useWishlistContext();
+  const { productDetails, closeProductModal, show } = useProductContext();
 
   if (show)
-  return (
-    <>
-      <ProductDetailModal
-        details={[productDetails]}
-        closeProductDetails={closeProductModal}
-      />
-      <button
-        className="z-50 absolute top-16 right-14 text-lg"
-        onClick={() => closeProductModal()}
-      >
-        <AiOutlineClose />
-      </button>
-    </>
-  );
+    return (
+      <>
+        <WishListProductDetailModal
+          details={productDetails}
+          closeProductDetails={closeProductModal}
+        />
+        <button
+          className="z-50 absolute top-16 right-14 text-lg"
+          onClick={() => closeProductModal()}
+        >
+          <AiOutlineClose />
+        </button>
+      </>
+    );
 
   if (wishListCart.length === 0) {
     return (
-      <div className="pt-[6.5rem]">
-        <h1>Empty WishList</h1>
-      </div>
+      <EmptyCartOrList
+        header="my wishlist"
+        pageName="wishlist"
+        heroIcon="FaHeart"
+      />
     );
   } else
     return (
@@ -53,29 +60,11 @@ const WishList = () => {
                   className="bg-white border p-3 shadow-sm rounded-sm"
                   key={index}
                 >
-                  <figure onClick={()=>{getProductDetails(item);openProductModal()}}>
-                    {/* {console.log(item)} */}
-                    <img
-                      src={item.image}
-                      alt=""
-                      className=" w-52 h-28 cursor-pointer lg:h-32"
-                    />
-                  </figure>
-                  <footer className="flex justify-center">
-                    <button
-                      className="text-center w-[90%] bg-orange-400 mt-2 p-1 hover:scale-105"
-                      onClick={() => {
-                        addToCart(item);
-                        removeFromWishList(item.id);
-                      }}
-                    >
-                      Add to Cart
-                    </button>
-                  </footer>
+                  <WishListItem item={item} />
                 </article>
               );
             })}
-            <NavLink to='/products'>
+            <NavLink to="/products">
               <article className="w-50 h-44 md:h-48 bg-white border rounded-sm shadow-sm flex flex-col justify-center items-center cursor-pointer select-none">
                 <IoAddOutline className="mt-4 text-5xl border-2 border-purple-600 rounded-full p-2 text-purple-400 border-dashed hover:scale-110" />
                 <button className="px-3 py-1 mt-5 border text-white bg-purple-600 rounded-full border-purple-600 font-semibold hover:scale-105">
