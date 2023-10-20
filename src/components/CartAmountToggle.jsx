10d8210ea7f2804b51e3d78df5d28745db40cc5d
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-20 11:16:56 +0530

    CartAmtToggle: added hover effect

--- src/components/CartAmountToggle.jsx
+++ src/components/CartAmountToggle.jsx
@@ -6,7 +6,7 @@ const CartAmountToggle = ({ newAmount,incrementAmount,decrementAmount}) => {
   return (
     <div className="flex justify-end gap-1 cursor-pointer font-medium">
       <span
-        className="bg-orange-300 rounded-full flex items-center justify-center p-2"
+        className="bg-orange-300 rounded-full flex items-center justify-center p-2 hover:scale-110"
         onClick={decrementAmount}
       >
         <AiOutlineMinus />
@@ -14,7 +14,7 @@ const CartAmountToggle = ({ newAmount,incrementAmount,decrementAmount}) => {
       <span className="text-xl flex items-center justify-center px-2 w-7 select-none">{newAmount}
       </span>
       <span
-        className="bg-orange-300 rounded-full flex items-center justify-center p-2"
+        className="bg-orange-300 rounded-full flex items-center justify-center p-2 hover:scale-110"
         onClick={incrementAmount}
       >
         <AiOutlinePlus />
