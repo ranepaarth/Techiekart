2bc4c1738bf3855d024b01d750ddc7e142bbe753
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-22 21:30:50 +0530

    implemented Auth0 login/signup/logout funtionality

--- src/components/Header/NavigationIcons.jsx
+++ src/components/Header/NavigationIcons.jsx
@@ -1,8 +1,10 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { FiShoppingBag, FiHeart, FiSettings } from "react-icons/fi";
+import { BsFillPersonCheckFill, BsFillPersonPlusFill } from "react-icons/bs";
 import { FaHeart } from "react-icons/fa";
-import { GoPerson } from "react-icons/go";
+import { FiHeart, FiSettings, FiShoppingBag } from "react-icons/fi";
+// import { GoPersonAdd, GoPersonFill } from "react-icons/go";
+import { NavLink } from "react-router-dom";
 import { useCartContext } from "../../context/cartContext";
 import { useWishlistContext } from "../../context/wishListContext";
 
@@ -10,6 +12,8 @@ const NavigationIcons = () => {
   const { cart } = useCartContext();
   const { wishListCart } = useWishlistContext();
   let wishListCartLength;
+
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
 
   if (wishListCart.length > 0) {
     wishListCartLength = true;
@@ -38,13 +42,34 @@ const NavigationIcons = () => {
               {wishListCart.length ? wishListCart.length : 0}
             </p>
           </div>
-          <span className="text-2xl lg:text-3xl">{wishListCartLength ? <FaHeart /> : <FiHeart />}</span>
+          <span className="text-2xl lg:text-3xl">
+            {wishListCartLength ? <FaHeart /> : <FiHeart />}
+          </span>
         </span>
       </NavLink>
       <div className="lg:w-[1px] lg:h-[20px] lg:bg-neutral-300"></div>
       <FiSettings />
       <div className="lg:w-[1px] lg:h-[20px] lg:bg-neutral-300"></div>
-      <GoPerson />
+      <span
+        className={`${
+          isAuthenticated ? "bg-green-500" : "bg-red-500"
+        } p-2 rounded-full`}
+      >
+        {" "}
+        {isAuthenticated ? (
+          <BsFillPersonCheckFill
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+            className="text-green-100"
+          />
+        ) : (
+          <BsFillPersonPlusFill
+            onClick={() => loginWithRedirect()}
+            className="text-red-100"
+          />
+        )}
+      </span>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -10,14 +11,22 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppProvider>
-      <FIlterContextProvider>
-        <CartContextProvider>
-          <WishlistContextProvider>
-          <App />
-          </WishlistContextProvider>
-        </CartContextProvider>
-      </FIlterContextProvider>
-    </AppProvider>
+    <Auth0Provider
+      domain="dev-uva2abrx125bdlr4.us.auth0.com"
+      clientId="FGw9TTfbvdu45W6ArUfuVkNk1xwkvCxN"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <AppProvider>
+        <FIlterContextProvider>
+          <CartContextProvider>
+            <WishlistContextProvider>
+              <App />
+            </WishlistContextProvider>
+          </CartContextProvider>
+        </FIlterContextProvider>
+      </AppProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
