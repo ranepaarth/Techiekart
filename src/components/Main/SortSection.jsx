f92d951886e7fdc4215324b66ff618765ef49617
ranepaarth <2019paarth.rane@ves.ac.in>
2023-10-30 13:14:27 +0530

    Removed comments and unnecessary function destructuring

--- src/components/Main/SortSection.jsx
+++ src/components/Main/SortSection.jsx
@@ -2,30 +2,9 @@ import React from "react";
 import { useFilterContext } from "../../context/filterContext";
 
 const SortSection = () => {
-  const { grid_view, setGridView, setListView, filter_products, sorting } =
-    useFilterContext();
-  // console.log(grid_view);
+  const { filter_products, sorting } = useFilterContext();
   return (
     <div className="text-sm flex justify-between items-center xl:px-28">
-      {/* <span className="flex bg-purple-50 p-[0.2rem] md:text-xl rounded-md border">
-        <button
-          className={`${
-            grid_view ? "bg-purple-600 text-white" : "text-purple-500"
-          } bg-purple-200 p-1 rounded-md`}
-          onClick={setGridView}
-        >
-          <BsGridFill />
-        </button>
-        <button
-          className={`
-            ${
-              grid_view ? "text-purple-600" : "bg-purple-600 text-purple-200"
-            } bg-purple-200 p-1 rounded-md`}
-          onClick={setListView}
-        >
-          <BsList />
-        </button>
-      </span> */}
       <span className="text-[10px] md:text-sm [font-weight:500] text-neutral-400">
         {filter_products.length} Product Available{" "}
       </span>
