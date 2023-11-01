f5c1f28914fb3f9dc4f8fe6aa591da395d21cbd0
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-01 22:04:17 +0530

    code refactor with reusable compo and props validation

--- src/components/WishListComponent/WishListProductDetailModal.jsx
+++ src/components/WishListComponent/WishListProductDetailModal.jsx
@@ -1,69 +1,63 @@
 import React from "react";
-
-//Icons
-import { AiOutlineDelete } from "react-icons/ai";
-import { IoBagAddOutline } from "react-icons/io5";
-
-//Context
-import { useProductContext } from "../../context/ProductContext";
 import { useCartContext } from "../../context/cartContext";
-import { useWishlistContext } from "../../context/wishlistContext";
 
-const ProductDetailModal = ({ details, closeProductDetails }) => {
+import ButtonClose from "../ButtonClose";
+import ProductTitle from "../ProductCard/ProductTitle";
+import ProductDescription from "../ProductDescription";
+import ProductDetailImage from "../ProductDetailImage";
+import ProductPrice from "../ProductPrice";
+import ProductRating from "../ProductRating";
+import WishListProdDetailFooter from "./WishListProdDetailFooter";
+
+import PropTypes from 'prop-types'
+
+const WishListProductDetailModal = ({ details }) => {
+  const { name, brand, image, rating, price, desc, id } = details;
+
+ 
+
   const { cart } = useCartContext();
-  const { addToCart, removeProduct } = useCartContext();
-  const { removeFromWishList } = useWishlistContext();
-  const { closeProductModal } = useProductContext();
 
   const productExists = cart.find((product) => {
     console.log(product.id === details.id);
     if (product.id === details.id) return true;
   });
 
-  // console.log(productExists)
-
-  let product;
+  console.log(details);
 
   if (!details) return;
   else
     return (
-      <main className="bg-neutral-100 w-screen h-screen absolute top-0 right-0 z-50">
-        <article className="flex flex-col justify-between bg-pink-100 md:bg-pink-200 lg:bg-pink-300 xl:bg-pink-400 absolute left-0 h-[90dvh] top-0 bottom-0 z-50 m right-0 mx-10 p-4 md:p-8 my-12">
-          <span className="hover:underline bg-white w-fit mt-6 p-2">
-            {details.title || details.name}
-          </span>
-          <footer className="p-3 flex flex-col gap-3">
-            <div className="flex justify-end">
-              <button
-                className="bg-red-400 p-1 text-white rounded-md border-[1px] border-red-800 hover:scale-110 w-fit"
-                onClick={() => {
-                  removeFromWishList(details.id);
-                  closeProductModal();
-                }}
-              >
-                <span className="flex items-center gap-2 p-1 capitalize">
-                  <AiOutlineDelete className="text-xl" />
-                  <p className="text-sm font-medium">remove from wishList</p>
-                </span>
-              </button>
-            </div>
-            <button
-              className="bg-orange-400 py-2 rounded-lg shadow-md text-center cursor-pointer capitalize hover:scale-105"
-              onClick={() => {
-                removeFromWishList(details.id);
-                addToCart(details);
-                closeProductDetails();
-              }}
-            >
-              <span className="flex items-center justify-center gap-2 font-medium">
-                <IoBagAddOutline className="text-xl" />
-                add to cart
-              </span>
-            </button>
-          </footer>
-        </article>
-      </main>
+        <section className="bg-black w-screen h-screen absolute top-0 right-0 bottom-0 z-50 flex justify-center items-center bg-opacity-80 px-10 cursor-default">
+
+          <article className="flex flex-col bg-white w-[500px] p-5 border shadow-md rounded-lg">
+            <ButtonClose />
+            <main className="flex flex-col h-full gap-4">
+              <ProductTitle
+                title={name}
+                brand={brand}
+                paddingTop={0}
+                text={"xl"}
+                font={"semibold"}
+                textCenter={"center"}
+                underline={"underline"}
+              />
+              <ProductDetailImage image={image} />
+
+              <ProductDescription description={desc} />
+
+              <ProductRating rating={rating} />
+
+              <ProductPrice amount={price} />
+            </main>
+            <WishListProdDetailFooter details={details} id={id} />
+          </article>
+        </section>
     );
 };
 
-export default ProductDetailModal;
+WishListProductDetailModal.propTypes = {
+  details: PropTypes.object
+}
+
+export default WishListProductDetailModal;
