e6f92506b56d61c63273f9dd8696007fa90eae22
ranepaarth <2019paarth.rane@ves.ac.in>
2023-11-01 22:10:43 +0530

    used the reusable components

--- src/components/ProductCard/Product.jsx
+++ src/components/ProductCard/Product.jsx
@@ -1,55 +1,22 @@
-//Components
-import ProductDetailModal from "./ProductDetailModal";
-
-//Icons
-
-//Contexts
-import { useProductContext } from "../../context/ProductContext";
-
-import DetailsIcon from "./DetailsIcon";
-import HeartIcon from "./HeartIcon";
 import ProductDeleteIcon from "./ProductDeleteIcon";
 import ProductFooter from "./ProductFooter";
-import ProductTitle from "./ProductTitle";
+import ProductPrice from "../ProductPrice";
+import ProductDetailImage from "../ProductDetailImage";
+import ProductHeader from "./ProductHeader";
 
 const Product = (currElem) => {
-  const { productDetails, show } = useProductContext();
-
-  if (show)
-    return (
-      <>
-        <ProductDetailModal details={productDetails} />
-      </>
-    );
+  const {title,category,brand,thumbnail,price,id} = currElem
 
   return (
     <div className="flex flex-col gap-4 relative select-none p-1 pt-0">
-      <span className="text-xs text-gray-400 absolute right-0 capitalize">
-        {currElem.category}
-      </span>
-      <header className="flex items-center justify-between">
-        <ProductTitle title={currElem.title} brand={currElem.brand} />
-        <span
-          className={`text-black text-xl  pt-6 cursor-pointer flex items-center gap-2`}
-        >
-          <DetailsIcon currElem={currElem} />
-          <HeartIcon currElem={currElem} />
-        </span>
-      </header>
+      <ProductHeader category={category} brand={brand} title={title} currElem={currElem}/>
       <main>
-        <figure className="p-2 bg-neutral-100 shadow-sm rounded-md border">
-          <img
-            src={currElem.thumbnail}
-            alt={currElem.title}
-            className="h-52 md:h-64 w-full rounded-md object-fill"
-          />
-        </figure>
-        <div className="flex justify-between items-center text-xl">
-          <span className="font-semibold py-3 text-">₹{currElem.price}</span>
-          <ProductDeleteIcon currElemId={currElem.id} />
+        <ProductDetailImage image={thumbnail} widthFull={"w-full"} mdHeight={"h-64"}/>
+        <div className="flex justify-between items-center mt-5 text-xl">
+          <ProductPrice amount={price} productCard={true}/>
+          <ProductDeleteIcon currElemId={id} />
         </div>
       </main>
-
       <footer>
         <div className="flex justify-end text-xl mb-1"></div>
         <ProductFooter currElem={currElem} />
